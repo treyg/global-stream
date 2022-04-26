d83432dbfa2fe1d710882f273aa5ede65122fb25
Trey Gordon <41927921+treyg@users.noreply.github.com>
2022-04-25 20:46:52 -0400

    Update endpoint

--- client/src/components/MediaCard.vue
+++ client/src/components/MediaCard.vue
@@ -97,7 +97,7 @@ export default {
     saveMedia() {
       //function to push prop data to watchlist
       axios
-        .post("http://68.183.129.164:3000/", {
+        .post("https://stream-notion-api.xyz:3000", {
           title: this.title,
           summary: this.summary,
           url: window.location.href,
