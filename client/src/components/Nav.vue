c6587dd892476d8d5f6151914f7518206c1908be
Trey Gordon <41927921+treyg@users.noreply.github.com>
2022-04-27 20:27:30 -0400

    Remove placeholder logo

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -1,11 +1,11 @@
 <template>
   <nav class="nav-wrapper">
     <ul id="nav-list">
-      <img
+      <!-- <img
         src="https://www.themoviedb.org/assets/2/v4/logos/408x161-powered-by-rectangle-green-bb4301c10ddc749b4e79463811a68afebeae66ef43d17bcfd8ff0e60ded7ce99.png"
         alt="logo"
         class="lg-only"
-      />
+      /> -->
       <div class="nav-list--li-wrap">
         <li>
           <router-link aria-label="Home." to="/"
