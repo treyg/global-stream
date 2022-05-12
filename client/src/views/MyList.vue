affdf96c6ca5707d0287da371ac2b64007daaa19
Trey Gordon <41927921+treyg@users.noreply.github.com>
2022-05-11 20:14:52 -0400

    Add my list route

---
+++ client/src/views/MyList.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+<h1>My List</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MyList',
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
