3348faa3158db609e0fae1dfdbc9f20cde3903f2
Trey Gordon <41927921+treyg@users.noreply.github.com>
2022-05-18 20:30:35 -0400

    Get list from local storage and print to list page

--- client/src/views/MyList.vue
+++ client/src/views/MyList.vue
@@ -1,15 +1,30 @@
 <template>
     <div>
-<h1>My List</h1>
+        <h1>My List</h1>
+        <div v-for="item in mediaList" :key="item.id">
+            <h2>{{ item.title }}</h2>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'MyList',
+export default {
+    name: 'MyList',
+    data() {
+        return {
+            mediaList: [],
+        }
+    },
+    methods: {
+        getMediaList() {
+            this.mediaList = JSON.parse(localStorage.getItem('mediaList'));
+        }
+    },
+    mounted() {
+        this.getMediaList();
     }
+}
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
 </style>
